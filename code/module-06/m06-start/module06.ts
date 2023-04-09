699512066c6d3c35bc1ad155cc8790d3e2f2300e
Diana Kalstein <dianakalstein@gmail.com>
2023-04-09 11:31:21 +0200

    Solved last task of lab6

--- code/module-06/m06-start/module06.ts
+++ code/module-06/m06-start/module06.ts
@@ -42,3 +42,12 @@ empIDs.AddOrUpdate(2, 89);
 console.log(empIDs.GetData(0));         // returns 50
 
 // TODO Test items as objects.
+type Pets = {
+    name: string;
+    breed: string;
+    age: number
+}
+let pets = new DataStore<Pets>();
+pets.AddOrUpdate(0, { name: 'Rex', breed: 'Golden Retriever', age: 5});
+pets.AddOrUpdate(1, { name: 'Sparky', breed: 'Jack Russell Terrier', age: 3});
+console.log(pets.GetData(1));         // returns { name: 'Sparky', breed: 'Jack Russell Terrier', age: 3 }
